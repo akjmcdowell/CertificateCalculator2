7f0c4eb227c080a712ae02d1dd95b7cec4285387
akjmcdowell <alastair.mcdowell@energyae.com>
2024-10-21 14:15:20 +1300

    oct21_1

--- Python/_Archive/cd.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-# Specify the path to ChromeDriver if not in PATH
-driver = webdriver.Chrome()  # Make sure this path is correct
-
-
-# Open a webpage
-driver.get('https://www.facebook.com')
-
